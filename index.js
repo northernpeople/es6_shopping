4e49192a73e08a1915fb63249deaf79711a07ec4
Stepan Razdobarov <northernpeople@gmail.com>
2018-10-18 12:09:35 -0600

    initial setup of a simple file server

---
+++ index.js
@@ -0,0 +1,24 @@
+let http = require("http");
+let fs = require("fs");
+let path = require("path");
+
+let handleError = (err, res) => {
+  res.writeHead(404);
+  res.end();
+}
+
+let server = http.createServer(function (req, res){
+  let fileName = req.url.length > 1? req.url.substring(1) : "index.html";
+
+  fs.readFile(
+    path.resolve(__dirname, "app", fileName),
+    function(err, content){
+      if(err) handleError(err, res);
+      else res.end(content);
+    }
+  )
+});
+
+
+console.log("listening es6 es6_shopping");
+server.listen(3031);
